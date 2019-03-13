5f2bf1e1bd01b14a53699f940d3c6b0fce0e58d1
Maurits Heetkamp <33858972+MauritsHeetkamp@users.noreply.github.com>
2019-03-13 23:19:56 +0100

    Collider Update
    Yes

--- VR Architecture Simulation/Assets/Scripts/Attributes.cs
+++ VR Architecture Simulation/Assets/Scripts/Attributes.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public static class Attributes
 {
+    public static List<GameObject> allChildren = new List<GameObject>();
     public static bool ToggleBool(this bool boolToToggle)
     {
         if (boolToToggle)
@@ -24,4 +25,24 @@ public static class Attributes
         }
         return start;
     }
+    public static GameObject[] GetAllChildren(this GameObject start, bool first = true)
+    {
+        if (first)
+        {
+            allChildren = null;
+            foreach (Transform child in start.transform)
+            {
+                allChildren.Add(child.gameObject);
+                GetAllChildren(child.gameObject, false);
+            }
+            return allChildren.ToArray();
+        }
+        foreach(Transform child in start.transform)
+        {
+            allChildren.Add(child.gameObject);
+            GetAllChildren(child.gameObject, false);
+        }
+        return null;
+    }
+
 }
